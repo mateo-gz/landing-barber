816fe531cfded4178314dae4bc16552d6dbeda09
Mateo Gomez <mr.mateo.oficial@gmail.com>
2025-06-26 10:59:34 -0600

    Home terminado, completamente

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './components/home'
+import './App.css'
+
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='hero'>
+        <h1 className="hero-title">
+            BARBER BROS
+        </h1>
+        <img className='hero-img' src="/img1.png"/>
+        {/* <p className="hero-description">
+            ✂️ Barber Bros Corte con actitud. Flow garantizado. Donde los hombres vienen a afilar su estilo. Degradados limpios, barbas precisas y el trato de hermanos.
+        </p> */}
+        <button className='hero-btn'>
+            Tu nuevo look empieza aquí.
+        </button>
+    </div>
+  )
+}
+
+export default Home
