f0fb3e3ac86bf7f7bfa485311070b20179510227
Mateo Gomez <mr.mateo.oficial@gmail.com>
2025-06-26 12:55:40 -0600

    mejorando la seccion de services

--- src/components/service.jsx
+++ src/components/service.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-const Service = ({title, desc, img, price}) => {
+const Service = ({title, img, price}) => {
   return (
     <div className='service'>
         <div className="service-title">{title}</div>
-        <p className="service-desc">{desc}</p>
         <img src={img} alt="service" className="service-img"></img>
         <div className="service-price">{price}</div>
         <button className='service-btn'>Reservar</button>
